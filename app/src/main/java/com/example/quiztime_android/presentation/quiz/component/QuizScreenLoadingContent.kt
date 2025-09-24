e69da787e8ab3246d86eb68f2c759a124d92bd98
Nagy Dániel <nagy.dani93@gmail.com>
2025-09-24 12:20:40 +0200

    Issue Report Screen

---
+++ app/src/main/java/com/example/quiztime_android/presentation/quiz/component/QuizScreenLoadingContent.kt
@@ -0,0 +1,50 @@
+package com.example.quiztime_android.presentation.quiz.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.quiztime_android.R
+
+@Composable
+fun QuizScreenLoadingContent(
+    modifier: Modifier = Modifier,
+    loadingMessage: String?,
+) {
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        CircularProgressIndicator()
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = loadingMessage.orEmpty(),
+            style = MaterialTheme.typography.titleLarge,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewQuizScreenLoadingContent() {
+    QuizScreenLoadingContent(
+        modifier = Modifier.fillMaxSize(),
+        loadingMessage = "Setting up Quiz...",
+    )
+}
